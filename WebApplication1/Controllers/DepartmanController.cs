360eafdd61139ffc4e102b69329210ec73bf04e5
melike nur karacabay <melikeenurky@gmail.com>
2023-02-10 18:07:22 +0300

    Add project files.

---
+++ WebApplication1/Controllers/DepartmanController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        public ActionResult Index()
+        {
+            Departman departman = new Departman();
+            { id = 1; Adi="Bilgi işlem" };
+            return View;
+        }
+        public String Index2()
+        {
+            return "merhaba Index2";
+        }
+        public ActionResult Index3()
+        {
+            Departman departman = new Departman();
+            { id = 1; Adi = "Bilgi işlem" };
+            return view(deparatman);
+        }
+    }
+}
---
+++ WebApplication1/Models/Departman.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Departman
+    {
+        public string index();
+        {id=1,adi="Bİlgi İşlem"};
+    }
+}
